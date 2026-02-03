1da69c3529b363c47c0b40acba2007b41c536e88
Paul Busby <paul@foxpaw.rs>
2026-02-03 21:58:52 +1100

    Feature :: Request - Updated cargo.toml to minimum standards for linting - Added http1::Request stub

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! # Habanero
+//!
+//! Rusty HTTP Client and Server ecosystem.
+//! Todo(Paul): Library documentation.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod http1;
